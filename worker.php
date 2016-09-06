ed61f4556ef447047aa8e6946d96335bf26b3a16
Ademola <styccs@gmail.com>
2016-09-06 14:41:18 +0100

    adding tps settings

--- worker.php
+++ worker.php
@@ -6,25 +6,16 @@ use PhpAmqpLib\Message\AMQPMessage;
 echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
 
 $callback = function($msg) {
-    $messagePublishedAt = $msg->body;
-    echo 'seconds between publishing and consuming: '
-        . (date('Y-m-d H:i:s', time()-$messagePublishedAt)) . PHP_EOL;
-//    $file = '/var/www/html/text.log';
-//    $current = file_get_contents($file);
-//    $current .= $msg->body;
-//    $current .= "\n";
-//    file_put_contents($file, $current);
+//    $messagePublishedAt = $msg->body;
+//    echo 'seconds between publishing and consuming: '
+//        . (date('Y-m-d H:i:s', time()-$messagePublishedAt)) . PHP_EOL;
+    $file = '/var/www/html/messages.log';
+    $current = file_get_contents($file);
+    $current .= $msg->body;
+    $current .= "\n";
+    file_put_contents($file, $current);
 };
 
-function process_message(AMQPMessage $message)
-{
-    $headers = $message->get('application_headers');
-    $nativeData = $headers->getNativeData();
-    var_dump($nativeData['x-delay']);
-    var_dump($nativeData);
-    var_dump($headers);
-//    $message->delivery_info['channel']->basic_nack($message->delivery_info['delivery_tag']);
-}
 
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 
@@ -33,7 +24,6 @@ $settings = require __DIR__.'/settings.php';
 $channel = $connection->channel();
 
 //$channel->basic_qos(null, 1, null);
-//$channel->basic_consume('delayed_queue', '', false, true, false, false, 'process_message');
 
 $channel->basic_consume($settings['queue_name'], '', false, true, false, false, $callback);
 
