ecd419ec8c2eefdd617210727de0292ca8955639
Ademola <styccs@gmail.com>
2016-09-07 17:53:04 +0100

    merge fixes

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
 foreach (range(1,10) as $x) {
-    $c = file_get_contents('http://localhost:9090?text=missing&from=abc&to=08170814752&smsc=goat');
+    $c = file_get_contents('http://sponge.atp-sevas.com/rabbitmq?text=TestMessage&from=5959&to=2348170814752&smsc=ETISALAT');
     var_dump($c);
 }
