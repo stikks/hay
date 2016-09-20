b3305be4e93f47246fc02ed37fcd122b9648c750
Ademola Oladipo <styccs@gmail.com>
2016-09-20 16:54:35 +0100

    adding response to 202 response

--- worker.php
+++ worker.php
@@ -6,12 +6,6 @@ $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 
 echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
 
-$settings = require __DIR__.'/settings.php';
-
-$channel = $connection->channel();
-
-$channel->basic_qos(null, 1, null);
-
 $callback = function($msg) {
 
     $headers = $msg->get('application_headers');
@@ -24,6 +18,12 @@ $callback = function($msg) {
     $data = '[DATETIME:'.$nativeData['timestamp'].'][STATUS: Accepted][SMSC:'.$nativeData['smsc'].'][FROM:'.$nativeData['from'].'][TO:'.$nativeData['to'].'][MSG:'.$msg->body.']';
     file_put_contents($file, $data.PHP_EOL, FILE_APPEND);
 };
+
+$settings = require __DIR__.'/settings.php';
+
+$channel = $connection->channel();
+
+$channel->basic_qos(null, 1, null);
 
 $channel->basic_consume($settings['queue_name'], '', false, true, false, false, $callback);
 
