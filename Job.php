76ccb83e9841a521d90b2aa2eaf9e518c3de05af
Ademola <styccs@gmail.com>
2017-03-10 18:26:40 +0100

    switched back to rabit

--- Job.php
+++ Job.php
@@ -26,12 +26,9 @@ class Job
 
             $recipients = $this->args['recipients'];
 
-            var_dump($recipients);
-            exit();
-
             foreach ($recipients as $rex) {
                 $headers = $this->args['headers'];
-                $headers['recipient'] = $rex;
+                $headers['recipient'] = $rex[0];
                 $headers['text'] = $this->args['text'];
 
                 $time = $service->get_timestamp($settings['tps']);
@@ -43,7 +40,7 @@ class Job
 //                $message->set('application_headers', $headers);
                 $channel->basic_publish($message, $settings['exchange_name'], $settings['queue_name']);
 
-                $data = '[DATETIME:'. time() .'][STATUS: Queued][SMSC:'. $headers['smsc'] .'][FROM:'.$headers['from'].'][TO:'.$rex.'][MSG:'.$headers['text'].'][DLR_MASK:'.$headers['dlr_mask'].'][DLR:'.$headers['dlr_url'].']';
+                $data = '[DATETIME:'. time() .'][STATUS: Queued][SMSC:'. $headers['smsc'] .'][FROM:'.$headers['from'].'][TO:'.$rex[0].'][MSG:'.$headers['text'].'][DLR_MASK:'.$headers['dlr_mask'].'][DLR:'.$headers['dlr_url'].']';
                 $log = new Logger($settings['logger']['name']);
                 $log->pushHandler(new StreamHandler($settings['logger']['path'], Logger::INFO));
                 $log->info($data);
